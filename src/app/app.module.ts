7fe190b71b540a5280a815922865f43932054a53
unknown <polishchuk.andrey2002@gmail.com>
2021-11-02 21:39:18 +0200

    made logo in side-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SideBarComponent} from "./side-bar/side-bar.component";
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
+    AppComponent,
+    SideBarComponent,
     AppComponent,
     HeaderComponent
   ],
