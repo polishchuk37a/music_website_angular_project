9e298c41f3317c3b81c58b9300fecdfd550d5285
unknown <polishchuk.andrey2002@gmail.com>
2021-11-04 11:26:37 +0200

    started to make main part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {SideBarComponent} from "./side-bar/side-bar.component";
-import { HeaderComponent } from './header/header.component';
+import {HeaderComponent} from './header/header.component';
+import {MainComponent} from './main/main.component';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SideBarComponent,
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SideBarComponent,
+        AppComponent,
+        HeaderComponent,
+        MainComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        SwiperModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
