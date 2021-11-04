e8a1913ff837b40db164c523b14c2c7ac3a14d97
unknown <polishchuk.andrey2002@gmail.com>
2021-11-04 18:42:42 +0200

    added swiper

--- src/app/main/all-albums/all-albums.component.ts
+++ src/app/main/all-albums/all-albums.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+import SwiperCore, { Pagination, Navigation } from "swiper";
+
+SwiperCore.use([Pagination, Navigation]);
 
 @Component({
   selector: 'app-all-albums',
   templateUrl: './all-albums.component.html',
-  styleUrls: ['./all-albums.component.scss']
+  styleUrls: ['./all-albums.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
+
 export class AllAlbumsComponent implements OnInit {
 
   constructor() { }
